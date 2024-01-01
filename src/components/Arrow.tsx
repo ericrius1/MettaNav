4ce5b533493e1e3e5e198853cc37591d145aa704
Eric <ericrius1@gmail.com>
2023-12-31 21:27:18 -0300

    prettier support

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,85 +1,114 @@
-import { Clone, useGLTF } from "@react-three/drei"
-import { useFrame, useThree } from "@react-three/fiber"
-import { useEffect, useLayoutEffect, useRef, useState } from "react"
+import { Clone, useGLTF } from '@react-three/drei'
+import { GroupProps, useFrame, useThree } from '@react-three/fiber'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { FlakesTexture } from 'three-stdlib'
-import { Mesh } from "three"
+import { Mesh } from 'three'
 import * as THREE from 'three'
-import { useControls } from "leva"
+import { useControls } from 'leva'
 import { StoreState, useStore } from '../store'
-import { motion } from "framer-motion-3d"
-import { damp } from "maath/easing"
+import { motion } from 'framer-motion-3d'
+import { damp } from 'maath/easing'
 
 type ArrowProps = {
-    direction: 'left' | 'right' | 'up' | 'down',
+  direction: 'left' | 'right' | 'up' | 'down'
 }
 
 const flakesTexture = new FlakesTexture() as HTMLCanvasElement
-
+const normalMap = new THREE.CanvasTexture(
+  flakesTexture as HTMLCanvasElement,
+  THREE.UVMapping,
+  THREE.RepeatWrapping,
+  THREE.RepeatWrapping
+)
+const normalScale = new THREE.Vector2(0.1, 0.1)
 export function Arrow({ direction, ...props }: ArrowProps) {
-    const { nodes, materials } = useGLTF('/models/arrow.glb')
-    const material = useRef<THREE.MeshStandardMaterial>(null)
-    const arrow = useRef<THREE.Group<THREE.Object3DEventMap>>(null)
-    const { width, height } = useThree((state) => state.viewport)
-    const setIconHovered = useStore((state: StoreState) => state.setIconHovered);
-    const [iconHovered, setIconHoveredLocal] = useState(false)
-    const { normalRepeat } = useControls('Arrow', {
-        normalRepeat: { value: 10, min: 1, max: 15, step: 1 },
-    })
-    useLayoutEffect(function setArrowDirection() {
-        if (direction === 'left') {
-            arrow.current.rotation.y = Math.PI / 2
-        }
-        if (direction === 'right') {
-            arrow.current.rotation.y = -Math.PI / 2
-        }
-        if (direction === 'up') {
-            arrow.current.rotation.x = Math.PI / 2
-        }
-        if (direction === 'down') {
-            arrow.current.rotation.x = -Math.PI / 2
-        }
-    })
-    useLayoutEffect(function setArrowMaterial() {
-        nodes.arrow.traverse((obj) => {
-            if (obj instanceof Mesh) {
-                obj.receiveShadow = obj.castShadow = true;
-            }
-        });
-        if (materials.arrow instanceof THREE.MeshStandardMaterial) {
-            materials.arrow.color.set('orange')
-            materials.arrow.normalMap = new THREE.CanvasTexture(flakesTexture as HTMLCanvasElement, THREE.UVMapping, THREE.RepeatWrapping, THREE.RepeatWrapping);
-            materials.arrow.normalMap.repeat.set(normalRepeat, normalRepeat);
-            materials.arrow.normalScale.set(.1, .1);
-            materials.arrow.roughness = 0.1
-            materials.arrow.metalness = 1.0
-        }
-    }, [normalRepeat])
+  const { nodes } = useGLTF('/models/arrow.glb')
+  const { normalRepeat } = useControls('Arrow', {
+    normalRepeat: { value: 10, min: 1, max: 15, step: 1 },
+  })
+  const material = useRef<THREE.MeshStandardMaterial>(null)
+  const arrow = useRef<GroupProps>(null)
+  const { width, height } = useThree((state) => state.viewport)
+  const setIconHovered = useStore((state: StoreState) => state.setIconHovered)
+  const [iconHovered, setIconHoveredLocal] = useState(false)
+  console.log('rerender')
+  const rotationSpeed = useRef(0)
 
-    function hoverStart() {
-        setIconHovered(true)
-        setIconHoveredLocal(true)
+  useLayoutEffect(function setArrowDirection() {
+    if (direction === 'left') {
+      arrow.current.rotation.y = Math.PI / 2
     }
-
-    function hoverEnd() {
-        setIconHovered(false)
-        setIconHoveredLocal(false)
+    if (direction === 'right') {
+      arrow.current.rotation.y = -Math.PI / 2
     }
-
-    const arrowVariants = {
-        hover: { scale: 1.2 },
-        unhover: { scale: 1 }
+    if (direction === 'up') {
+      arrow.current.rotation.x = Math.PI / 2
     }
+    if (direction === 'down') {
+      arrow.current.rotation.x = -Math.PI / 2
+    }
+  })
+  useLayoutEffect(
+    function setArrowMaterial() {
+      nodes.arrow.traverse((obj) => {
+        if (obj instanceof Mesh) {
+          obj.receiveShadow = obj.castShadow = true
+        }
+      })
+    },
+    [normalRepeat]
+  )
 
-    useFrame((state, delta) => {
-        damp(material.current, 'envMapIntensity', iconHovered ? 5 : .5, 0.1, delta)
+  function hoverStart() {
+    setIconHovered(true)
+    setIconHoveredLocal(true)
+  }
 
-    })
+  function hoverEnd() {
+    setIconHovered(false)
+    setIconHoveredLocal(false)
+  }
 
-    return (
-        <motion.group {...props} ref={arrow} position={[direction === 'right' ? -width / 2.5 : width / 2.5, -height / 2.5, 0]} onPointerEnter={hoverStart} onPointerLeave={hoverEnd} variants={arrowVariants} whileHover="hover" initial="unhover">
-            <Clone object={nodes.arrow}>
-                <meshStandardMaterial {...materials.arrow} ref={material} />
-            </Clone>
-        </motion.group>
-    )
+  const arrowVariants = {
+    hover: { scale: 1.2 },
+    unhover: { scale: 1 },
+  }
+
+  useFrame((state, delta) => {
+    if (material.current) {
+      damp(material.current, 'envMapIntensity', iconHovered ? 3 : 0.7, 0.1, delta)
+    }
+    damp(rotationSpeed, 'current', iconHovered ? 0.001 : 0, 0, delta, 1)
+
+    if (arrow.current && arrow.current.rotation) {
+      //@ts-ignore
+      arrow.current.rotation.z += rotationSpeed.current
+    }
+  })
+
+  return (
+    <motion.group
+      {...props}
+      ref={arrow}
+      position={[direction === 'right' ? -width / 2.5 : width / 2.5, -height / 2.5, 0]}
+      onPointerEnter={hoverStart}
+      onPointerLeave={hoverEnd}
+      variants={arrowVariants}
+      whileHover='hover'
+      initial='unhover'
+    >
+      <Clone object={nodes.arrow}>
+        <meshStandardMaterial
+          normalMap={normalMap}
+          normalMap-repeat={normalRepeat}
+          normalScale={normalScale}
+          roughness={0.1}
+          color={'orange'}
+          metalness={1}
+          ref={material}
+        />
+      </Clone>
+    </motion.group>
+  )
 }
+useGLTF.preload('/models/arrow.glb')
